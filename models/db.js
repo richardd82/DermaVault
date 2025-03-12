18a54870daab2bd39ebb6ef99f0dab70c34340ba
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-03-12 00:57:00 -0600

    MODIFY db.js to use DATABASE_URL for Sequelize connection

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ let sequelize;
 if (config.use_env_variable) {
   // sequelize = new Sequelize(process.env[config.use_env_variable], config);
   console.log("DATABASE_URL:", process.env.DATABASE_URL);
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: config.dialect,
     dialectOptions: {
       ssl: {
