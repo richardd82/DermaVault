335d5f72de6d3fbe8a30b5f2f67ad2d68cd3f4d7
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-03-12 01:01:57 -0600

    MODIFY db.js to comment out Sequelize connection logic

--- models/db.js
+++ models/db.js
@@ -4,9 +4,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
 let sequelize;
-if (config.use_env_variable) {
+// if (config.use_env_variable) {
   // sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
+  // console.log("DATABASE_URL:", process.env.DATABASE_URL);
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: config.dialect,
     dialectOptions: {
@@ -16,8 +16,8 @@ if (config.use_env_variable) {
       }
     }
   });
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 module.exports = sequelize;
