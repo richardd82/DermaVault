57d99ede87c8646b4bb5c2d952857dbb05958bbb
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-03-18 03:24:44 -0600

    MODIFY auth route to send a complet user JSON

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,35 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
 
 // POST /api/auth/login - Iniciar sesión
-router.post('/login', async (req, res) => {  
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-console.log(password, "ESTE ES MI PASSWORD")
+  // console.log(password, "ESTE ES MI PASSWORD")
   try {
     const user = await User.findOne({ where: { username } });
 
     if (!user) {
-      return res.status(401).json({ message: 'Usuario no encontrado' });
+      return res.status(401).json({ message: "Usuario no encontrado" });
     }
 
     const isValid = await user.validPassword(password);
     if (!isValid) {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
+      return res.status(401).json({ message: "Contraseña incorrecta" });
     }
 
+    user.last_login = new Date();
+    await user.save();
+    
     const token = jwt.sign(
       { id: user.id, username: user.username },
-      'tu_secreto_seguro', // Cambia esto por una variable segura en producción
-      { expiresIn: '4h' }
+      "tu_secreto_seguro", // Cambia esto por una variable segura en producción
+      { expiresIn: "4h" }
     );
 
-    res.json({ token });
-
+    res.json({
+      id: user.id, // <-- Asegúrate de incluirlo
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      last_login: user.last_login,
+      is_active: user.is_active,
+      token,
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    res.status(500).json({ message: "Error en el servidor" });
   }
 });
-module.exports = router; 
+module.exports = router;
