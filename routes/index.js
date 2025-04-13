221f1dbe9eaeadcf3be7e9dce6900c89fdd98c52
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-04-12 21:09:48 -0600

    CREATE Endpoint to charge 27 thousand chunk in 25 registers block

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const patientRoutes = require('./patientRoutes');
 const medicalHistoryRoutes = require('./medicalHistoryRoutes'); // Si tienes un archivo de rutas para MedicalHistory
+const patientsArray = require('./patientsArrayChunks'); // Si tienes un archivo de rutas para pacientesArrayChunks
 
 // Agregar las rutas individuales
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/patients', patientRoutes);
 router.use('/histories', medicalHistoryRoutes);
+router.use('/patientsArrayChunks', patientsArray); // Si tienes un archivo de rutas para pacientesArrayChunks
 // Si tuvieras otras rutas, por ejemplo, para MedicalHistory, las agregarías así:
 // router.use('/medical-histories', require('./medicalHistoryRoutes'));
 
