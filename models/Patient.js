8b80bdfedc0e85b94ee74c64379d09e66c41743e
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-04-14 03:30:18 -0600

    MODIFY Patient name and direccion to TYPE TEXT and change Sync db on server.js to true

--- models/Patient.js
+++ models/Patient.js
@@ -12,7 +12,7 @@ const Patient = sequelize.define(
     },
     cedula: { type: DataTypes.STRING(50), allowNull: false, unique: true },
     apellido: { type: DataTypes.STRING(100), allowNull: false },
-    nombre: { type: DataTypes.STRING(100), allowNull: false },
+    nombre: { type: DataTypes.TEXT, allowNull: false },
     fecha_nacimiento: { type: DataTypes.DATEONLY, allowNull: true},
     edad: { type: DataTypes.INTEGER },
     lugar_nacimiento: { type: DataTypes.STRING(100) },
@@ -24,7 +24,7 @@ const Patient = sequelize.define(
     telefono_movil: { type: DataTypes.STRING(150) },
     email: { type: DataTypes.STRING(150), validate: { isEmail: true } },
     referido_por: { type: DataTypes.STRING(150) },
-    direccion: { type: DataTypes.STRING(255) },
+    direccion: { type: DataTypes.TEXT },
     created_by: { 
       type: DataTypes.INTEGER.UNSIGNED,
       allowNull: true // O false si quieres forzar que siempre exista el usuario creador
