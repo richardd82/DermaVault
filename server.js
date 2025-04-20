afb293c1d20ef6cada532458e5580943cc1a451c
Ricardo Díaz <49500398+richardd82@users.noreply.github.com>
2025-04-20 07:51:37 -0600

    CHANGE Cors credentials to false

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'https://derma.richadd82.dev/'], // o mejor el dominio del frontend: 'http://localhost:5173' o 'https://tudominio.com'
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  credentials: false
 }));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '4096mb' }));
